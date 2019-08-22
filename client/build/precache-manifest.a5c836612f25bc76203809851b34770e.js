8ee92cad617842a47fbb3ab88752daf38a0b4c65
kat-lagnado <ezlagnado@gmail.com>
2019-08-22 15:05:38 -0400

    frontend

---
+++ client/build/precache-manifest.a5c836612f25bc76203809851b34770e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c64e5c69723e2d99649c9af41abd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee1c9d55058f8f127ab",
+    "url": "/static/css/main.eae7e646.chunk.css"
+  },
+  {
+    "revision": "830db68935d6e1f2e1dc",
+    "url": "/static/js/2.8a430dfb.chunk.js"
+  },
+  {
+    "revision": "aee1c9d55058f8f127ab",
+    "url": "/static/js/main.7ad908e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
